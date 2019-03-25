56a58813f48cabfe79e7c8ccbd1ee7dcee4b90e5
wecode <umurangamclaire@gmail.com>
2019-03-25 10:54:38 +0200

    working on the email txt and templates

--- pictures/email.py
+++ pictures/email.py
@@ -3,12 +3,12 @@ from django.template.loader import render_to_string
 
 def send_welcome_email(name,receiver):
     # Creating message subject and sender
-    subject = 'Welcome to the MoringaTribune NewsLetter'
-    sender = 'james@moringaschool.com'
+    subject = 'Welcome to my Instagram Clone app'
+    sender = 'angelakarenzi5@gmail.com'
 
     #passing in the context vairables
-    text_content = render_to_string('email/newsemail.txt',{"name": name})
-    html_content = render_to_string('email/newsemail.html',{"name": name})
+    text_content = render_to_string('email/picturesemail.txt',{"name": name})
+    html_content = render_to_string('email/picturesemail.html',{"name": name})
 
     msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
     msg.attach_alternative(html_content,'text/html')
